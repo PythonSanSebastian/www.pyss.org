38b2f783b113cad2c6237494dc273416dbc2d0a1
Oier <badtrex@gmail.com>
2019-07-11 18:33:00 +0200

    primer test con nikola

--- _scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-#then
-#    cd _site || exit
-#    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" . .
-#fi
-
-ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" ./_site .
---
+++ www.pyss.org/pages/index.rst
@@ -0,0 +1,10 @@
+.. title: index
+.. slug: index
+.. date: 2019-07-11 18:12:37 UTC+02:00
+.. tags: 
+.. category: 
+.. link: 
+.. description: 
+.. type: text
+
+Write your page here.

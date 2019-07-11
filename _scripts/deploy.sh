08ea0dc84dbbfe92e22475b399c09c2d5b94e54e
Oier <badtrex@gmail.com>
2019-07-11 15:57:41 +0200

    algun dia funcionara

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-then
-    cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" . .
-fi
+#if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
+#then
+#    cd _site || exit
+#    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" . .
+#fi
+
+ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" . _site
